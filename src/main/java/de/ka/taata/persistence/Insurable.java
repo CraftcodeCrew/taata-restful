8b38a4a00d6d6efce80bad313cf75d77aae7a91f
luthesebas <luthesebas@gmail.com>
2018-10-27 13:44:46 +0200

    Implemented Insurable Model

---
+++ src/main/java/de/ka/taata/persistence/Insurable.java
@@ -0,0 +1,46 @@
+package de.ka.taata.persistence;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+/**
+ *
+ */
+@Data
+@Entity
+@EqualsAndHashCode(of = "id")
+@Table(name = "insurable")
+public class Insurable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String imageId;
+
+    private boolean isInsured;
+    private double probability;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private Insurance insurance;
+
+    //--------------------------------------
+    // Constructors
+    //--------------------------------------
+
+    public Insurable() {
+
+    }
+
+    public Insurable(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    //--------------------------------------
+    // Methods
+    //--------------------------------------
+
+}
---
+++ src/main/java/de/ka/taata/persistence/InsurableDAO.java
@@ -0,0 +1,10 @@
+package de.ka.taata.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ */
+public interface InsurableDAO extends JpaRepository<Insurable, Long> {
+
+}
