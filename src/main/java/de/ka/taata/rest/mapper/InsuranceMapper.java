ba5d48c790ec41f5a2b516aa70cdd0125b58385c
luthesebas <luthesebas@gmail.com>
2018-10-28 02:34:31 +0200

    Removed unused imports

--- src/main/java/de/ka/taata/rest/mapper/InsuranceMapper.java
+++ src/main/java/de/ka/taata/rest/mapper/InsuranceMapper.java
@@ -1,7 +1,6 @@
 package de.ka.taata.rest.mapper;
 
 import de.ka.taata.persistence.Insurance;
-import de.ka.taata.rest.CategoryController;
 import de.ka.taata.rest.InsuranceController;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
